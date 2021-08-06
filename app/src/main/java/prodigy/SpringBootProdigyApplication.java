039e3111746cd89916ebb1e6660b68a261ca0461
Mayank Mohit <mayank.mohit1122@gmail.com>
2021-08-06 16:47:38 +0530

    Initial Structure

---
+++ app/src/main/java/prodigy/SpringBootProdigyApplication.java
@@ -0,0 +1,13 @@
+package prodigy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootProdigyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootProdigyApplication.class, args);
+	}
+
+}
